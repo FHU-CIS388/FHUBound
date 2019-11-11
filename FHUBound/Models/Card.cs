4c118be185cf12501fd095535af6462f72e31a20
Kenan Casey <kenancasey@gmail.com>
2019-11-11 16:55:57 -0600

    Add missing pages; Fix file names for Android
    Various UI improvements

--- FHUBound/Models/Card.cs
+++ FHUBound/Models/Card.cs
@@ -3,20 +3,22 @@ namespace FHUBound.Models
 {
     public class Card
     {
-        public string HeaderTitle { get; set; }
-        public string HeaderSubtitle { get; set; }
-        public string ImageUrl { get; set; }
-        public string VideoUrl { get; set; }
+        public string HeaderTitle { get; set; } = "";
+        public string HeaderSubtitle { get; set; } = "";
+        public string ImageUrl { get; set; } = "";
+        public string VideoUrl { get; set; } = "";
 
-        public string ButtonUrl { get; set; }
-        public string ButtonText { get; set; }
+        public string ButtonUrl { get; set; } = "";
+        public string ButtonText { get; set; } = "";
 
-        public string BodyTitle { get; set; }
-        public string BodySubtitle { get; set; }
-        public string BodyContent { get; set; }
+        public string BodyTitle { get; set; } = "";
+        public string BodySubtitle { get; set; } = "";
+        public string BodyContent { get; set; } = "";
 
-        public DateTime StartDateTime { get; set; }
+        public DateTime StartDateTime { get; set; } 
         public DateTime EndDateTime { get; set; }
+
+        public int Value { get; set; }
 
         public Card()
         {
--- FHUBound/Views/HomePage.xaml.cs
+++ FHUBound/Views/HomePage.xaml.cs
@@ -15,11 +15,13 @@ namespace FHUBound.Views
             InitializeComponent();
 
             Cards = new ObservableCollection<Card>();
-            Cards.Add(new Card() { HeaderTitle = "Find Your Fit", HeaderSubtitle = "MAJORS", ImageUrl= "istockpuzzle.jpg", ButtonText="Explore Majors" });
-            Cards.Add(new Card() { HeaderTitle = "Devos with David", HeaderSubtitle = "DEVO", ButtonText="Watch More"});
-            Cards.Add(new Card() { HeaderTitle = "RUSH 2020", HeaderSubtitle = "Experience new spirital heights." , ImageUrl = "Rush.png", ButtonText="REGISTER"});
-
+            Cards.Add(new Card() { HeaderTitle = "Find Your Fit", HeaderSubtitle = "MAJORS", ImageUrl= "istockpuzzle.jpg", ButtonText="Explore Majors", Value = 5 } );
+            Cards.Add(new Card() { HeaderTitle = "Devos with David", HeaderSubtitle = "DEVO", ButtonText="Watch More", Value = 10 });
+            Cards.Add(new Card() { HeaderTitle = "RUSH 2020", HeaderSubtitle = "Experience new spirital heights." , ImageUrl = "Rush.png", ButtonText="REGISTER", Value = 25 });
+            Cards.Add(new Card() { BodyTitle="TIP #45", BodyContent="Hang out with friends at Eno park. #hammockJokes", Value = 5 });
+            
             CardsCollectionView.ItemsSource = Cards;
+
         }
     }
 }
