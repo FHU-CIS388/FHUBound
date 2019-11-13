d64cd3ff21831f17798168cb7367a41c95ffcb38
Kenan Casey <kenancasey@gmail.com>
2019-11-13 16:57:42 -0600

    Fix android bottom margin issue.
    Add image

--- FHUBound/Views/HomePage.xaml.cs
+++ FHUBound/Views/HomePage.xaml.cs
@@ -19,7 +19,9 @@ namespace FHUBound.Views
             Cards.Add(new Card() { HeaderTitle = "Devos with David", HeaderSubtitle = "DEVO", ButtonText="Watch More", Value = 10 });
             Cards.Add(new Card() { HeaderTitle = "RUSH 2020", HeaderSubtitle = "Experience new spirital heights." , ImageUrl = "Rush.png", ButtonText="REGISTER", Value = 25 });
             Cards.Add(new Card() { BodyTitle="TIP #45", BodyContent="Hang out with friends at Eno park. #hammockJokes", Value = 5 });
-            
+            Cards.Add(new Card() { HeaderTitle = "So Much Fun", HeaderSubtitle = "FHU FUN", ImageUrl="fhu_color_belltower.png", Value = 5 });
+
+
             CardsCollectionView.ItemsSource = Cards;
 
         }
