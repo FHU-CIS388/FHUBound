a2500727eea7eafce21496051cae784c17333f9f
Kenan Casey <kenancasey@gmail.com>
2019-11-13 15:56:41 -0600

    Move android images; update card

--- FHUBound/Models/Card.cs
+++ FHUBound/Models/Card.cs
@@ -5,15 +5,22 @@ namespace FHUBound.Models
     {
         public string HeaderTitle { get; set; } = "";
         public string HeaderSubtitle { get; set; } = "";
+        public string HeaderBackgroundColor { get; set; } = "";
+        public string HeaderForegroundColor { get; set; } = "";
+
         public string ImageUrl { get; set; } = "";
         public string VideoUrl { get; set; } = "";
-
-        public string ButtonUrl { get; set; } = "";
-        public string ButtonText { get; set; } = "";
 
         public string BodyTitle { get; set; } = "";
         public string BodySubtitle { get; set; } = "";
         public string BodyContent { get; set; } = "";
+        public string BodyBackgroundColor { get; set; } = "";
+        public string BodyForegroundColor { get; set; } = "";
+
+        public string ButtonUrl { get; set; } = "";
+        public string ButtonText { get; set; } = "";
+        public string ButtonBackgroundColor { get; set; } = "";
+        public string ButtonForegroundColor { get; set; } = "";
 
         public DateTime StartDateTime { get; set; } 
         public DateTime EndDateTime { get; set; }
