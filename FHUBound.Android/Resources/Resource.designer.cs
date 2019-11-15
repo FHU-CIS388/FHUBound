5f695a90d7e414be1b9c4534cf81968c0fc3bd03
jjcannon52 <43794674+jjcannon52@users.noreply.github.com>
2019-11-15 02:07:29 -0600

    initial image issue fixes and profile page work

--- FHUBound.Android/Resources/Resource.designer.cs
+++ FHUBound.Android/Resources/Resource.designer.cs
@@ -5749,6 +5749,7 @@ namespace FHUBound.Droid
 			public const int fhulogomaroon_400 = 2131165288;
 			
 			// aapt resource value: 0x7F070066
+			public const int fhu_color_belltower = 2131165286;
 			
 			// aapt resource value: 0x7F070067
 			public const int fhu_square = 2131165287;
@@ -5907,19 +5908,22 @@ namespace FHUBound.Droid
 			public const int Rush = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tab_about = 2131165340;
+			public const int second = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int tab_feed = 2131165341;
+			public const int tab_about = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int tooltip_frame_dark = 2131165342;
+			public const int tab_feed = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_light = 2131165343;
+			public const int tooltip_frame_dark = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int xamarin_logo = 2131165344;
+			public const int tooltip_frame_light = 2131165344;
+			
+			// aapt resource value: 0x7F0700A1
+			public const int xamarin_logo = 2131165345;
 			
 			static Drawable()
 			{
