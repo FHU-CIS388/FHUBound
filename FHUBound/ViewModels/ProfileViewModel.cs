4778df9f736332bd2a973d355ee2719db81479e4
Joshua Steward <31291387+JoshuaSteward1193@users.noreply.github.com>
2019-11-18 16:31:41 -0600

    Profile Page work

---
+++ FHUBound/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FHUBound.ViewModels
+{
+    class ProfileViewModel : BaseViewModel
+    {
+
+        public ProfileViewModel()
+        {
+
+        }
+    }
+}
