469f8ec506e55f287351090f4fb5d5c120e31091
cameron-maguffee <31316379+cameron-maguffee@users.noreply.github.com>
2019-11-19 18:06:43 -0600

    Profile page

--- FHUBound/Views/ProfilePage.xaml.cs
+++ FHUBound/Views/ProfilePage.xaml.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using FHUBound.Models;
 using Xamarin.Forms;
 
 namespace FHUBound.Views
 {
     public partial class ProfilePage : ContentPage
     {
+        public ObservableCollection<Card> Cards { get; set; }
+
         public ProfilePage()
         {
             InitializeComponent();
+
+            Cards = new ObservableCollection<Card>();
+            Cards.Add(new Card() { HeaderTitle = "Profile Pic", ImageUrl = "fhu_square.jpg", ButtonText = "Change Pic"});
+            Cards.Add(new Card() { HeaderTitle = "Badges Collected"});
+
+
+            CardsCollectionView.ItemsSource = Cards;
+
         }
     }
 }
