3092d37827640589be672b4eefdc23cd98ba806f
trentonblack <36003301+trentonblack@users.noreply.github.com>
2019-11-19 21:51:23 -0600

    Made popups and put picture into the store.

--- FHUBound/Views/StorePage.xaml.cs
+++ FHUBound/Views/StorePage.xaml.cs
@@ -7,6 +7,11 @@ namespace FHUBound.Views
 {
     public partial class StorePage : ContentPage
     {
+        async void OnDisplayActionSheetButtonClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Confirm?",  "No", "Yes");
+            Console.WriteLine("Action: " + action);
+        }
         public StorePage()
         {
             InitializeComponent();
