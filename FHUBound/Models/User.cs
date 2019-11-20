27e26384647c2fbff9200e6d3d10d9b59ee2c27b
Kenan Casey <kenancasey@gmail.com>
2019-11-20 16:29:25 -0600

    Add Profile Page Sample
    Update XF

---
+++ FHUBound/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FHUBound.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public int CurrentPoints { get; set; }
+        public string Email { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
---
+++ FHUBound/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FHUBound.Models;
+
+namespace FHUBound.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public User User { get; set; }
+        public string Message { get; set; }
+
+        public ProfileViewModel()
+        {
+            User = App.CurrentUser;
+            Message = "Good Morning, " + User.Username;
+        }
+    }
+}
