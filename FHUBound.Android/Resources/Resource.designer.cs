5c17036e576ab8c8ab37e9aa5d36b15e0e107d4f
jjcannon52 <43794674+jjcannon52@users.noreply.github.com>
2019-11-21 10:17:47 -0600

    update cookie
    add cookie clicker page and work on making it navbar accessible

--- FHUBound.Android/Resources/Resource.designer.cs
+++ FHUBound.Android/Resources/Resource.designer.cs
@@ -5911,19 +5911,22 @@ namespace FHUBound.Droid
 			public const int second = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int tab_about = 2131165341;
+			public const int ShannonSticker = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int tab_feed = 2131165342;
+			public const int tab_about = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_dark = 2131165343;
+			public const int tab_feed = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int tooltip_frame_light = 2131165344;
+			public const int tooltip_frame_dark = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int xamarin_logo = 2131165345;
+			public const int tooltip_frame_light = 2131165345;
+			
+			// aapt resource value: 0x7F0700A2
+			public const int xamarin_logo = 2131165346;
 			
 			static Drawable()
 			{
