12c8e35ff9bdbcbc14119bb5eb8454c8c6c703e2
cameron-maguffee <31316379+cameron-maguffee@users.noreply.github.com>
2019-12-04 07:37:18 -0600

    Home page

--- FHUBound/Views/HomePage.xaml.cs
+++ FHUBound/Views/HomePage.xaml.cs
@@ -15,12 +15,11 @@ namespace FHUBound.Views
             InitializeComponent();
 
             Cards = new ObservableCollection<Card>();
-            Cards.Add(new Card() { HeaderTitle = "Find Your Fit", HeaderSubtitle = "MAJORS", ImageUrl= "istockpuzzle.jpg", ButtonText="Explore Majors", Value = 5 } );
-            Cards.Add(new Card() { HeaderTitle = "Devos with David", HeaderSubtitle = "DEVO", ButtonText="Watch More", Value = 10 });
-            Cards.Add(new Card() { HeaderTitle = "RUSH 2020", HeaderSubtitle = "Experience new spirital heights." , ImageUrl = "Rush.png", ButtonText="REGISTER", Value = 25 });
-            Cards.Add(new Card() { BodyTitle="TIP #45", BodyContent="Hang out with friends at Eno park. #hammockJokes", Value = 5 });
-            Cards.Add(new Card() { HeaderTitle = "So Much Fun", HeaderSubtitle = "FHU FUN", ImageUrl="fhu_color_belltower.png", Value = 5 });
 
+            Cards.Add(new Card() { HeaderTitle = "RUSH 2020", HeaderSubtitle = "Experience new spirital heights.", ImageUrl = "Rush.png", ButtonText = "REGISTER", Value = 25 });
+            Cards.Add(new Card() { HeaderTitle = "So Much Fun", HeaderSubtitle = "FHU FUN", ImageUrl = "fhu_color_belltower.png", Value = 5 });
+            Cards.Add(new Card() { HeaderTitle = "Find Your Fit", HeaderSubtitle = "MAJORS", ImageUrl= "istockpuzzle.jpg", ButtonText="Explore Majors", Value = 5 } );
+            Cards.Add(new Card() { BodyTitle="TIP #45", BodyContent="Hang out with friends at Eno park. #hammockJokes", Value = 5 });
 
             CardsCollectionView.ItemsSource = Cards;
 
